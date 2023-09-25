6ca94106e53ca92bc6a1bbb9aa976f978e7fe51f
scinkir <scinkir@turksat.com.tr>
2023-09-25 09:48:33 +0300

    first commit

---
+++ src/test/java/tr/com/staj/stajapigateway/StajApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.staj.stajapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StajApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
